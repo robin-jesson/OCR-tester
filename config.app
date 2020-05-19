d4ae0776294ab48a5456ab55317f5bb8a1da5dcb
robin-jesson <62558641+robin-jesson@users.noreply.github.com>
2020-05-19 19:14:06 +0200

    essai ajout tableau csv dans ihm

--- config.app
+++ config.app
@@ -1,2 +1,2 @@
-#Tue May 19 11:45:39 CEST 2020
+#Tue May 19 16:18:50 CEST 2020
 pathToData=C\:\\Users\\robin.jesson\\ALTEN Group\\[STAGE] App Mobile - General\\AppTester\\OCRTester\\test.txt
--- src/main/java/com/alten/user_io/FirstWindow.java
+++ src/main/java/com/alten/user_io/FirstWindow.java
@@ -6,17 +6,33 @@
 package com.alten.user_io;
 
 import com.alten.ocrtester.preferences.AppPreferences;
+import com.alten.test_data.CV;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.WindowConstants;
 import javax.swing.JFileChooser;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 /**
  *
@@ -28,6 +44,8 @@ public class FirstWindow extends JFrame {
     public static final int HEIGHT = 200; 
 
     public FirstWindow() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
         UserIOAppInterface uioInterface = new UserIOAppInterface();
         AppPreferences pref = AppPreferences.getInstance();
         
@@ -58,16 +76,29 @@ public class FirstWindow extends JFrame {
         partialTestBtn.setBounds(0, 50, 200, 50);
         this.add(partialTestBtn);
 
+        JTable tableres = new JTable();
+        tableres.setBounds(0, 200, 1000, 300);
+        this.add(tableres);
+        
         JLabel pathToRes = new JLabel();
-        pathToRes.setBounds(210, 100, 200, 50);
+        pathToRes.setBounds(210, 50, 100, 50);
+        pathToRes.setText("xcsc");
         this.add(pathToRes);
         
         JButton fullTestBtn = new JButton("Lancer un test complet");
         fullTestBtn.setBounds(0, 100, 200, 50);
         fullTestBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                
                 String res = uioInterface.testDataFile();
-                pathToRes.setText(Paths.get(res).getFileName().toString());
+                if(res == null) {
+                    JOptionPane.showMessageDialog(null, "Veuillez reconfigurer le fichier test.");
+                    return;
+                }
+                System.out.println(res);
+                JTable tablenew = getJTableFomCsv(res);
+                
+                tableres.setModel(tablenew.getModel());
             }
         });
         this.add(fullTestBtn);
@@ -75,5 +106,28 @@ public class FirstWindow extends JFrame {
         this.setSize(600, 300);
         this.setLayout(null);
         this.setVisible(true);
+    }
+    
+    private static JTable getJTableFomCsv(String file){
+        Path path = Paths.get(file);
+        String header = "";
+        Object[][] elts = null;
+        Object[] head = null;
+        try{
+            List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+            head = lines.remove(0).split(";");
+            elts = new Object[lines.size()][head.length];
+            for(int i=0;i<lines.size();i++){
+                String[] eltLine = lines.get(i).split(";");
+                for(int j=0;j<eltLine.length;j++){
+                    elts[i][j] = eltLine[j];
+                }
+            }
+        }
+        catch(Exception e){
+            System.err.println(e);
+        }
+        JTable table = new JTable(elts,head);
+        return table;
     }
 }
